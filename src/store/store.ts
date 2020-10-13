e2424f972168978b8be877397c8e2f3f0c29f037
Eddy Rivera <eddyrivera@ITs-MacBook-Pro.local>
2020-10-13 11:28:44 -0300

    Set redux devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,8 @@
-import { createStore } from 'redux';
-import reducers from './combinedReducers'
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import createSagaMiddleware from 'redux-saga';
+import reducers from './combinedReducers';
 
-export default createStore(reducers);
+// const sagaMiddleware = createSagaMiddleware();
+
+export default createStore(reducers, composeWithDevTools(applyMiddleware()));
